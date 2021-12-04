593de53dde3d85a331bf474aa3f7979193500d01
Nurik Turdukulov <nurikturdukulov@Nurikui-MacBookPro.local>
2021-12-05 01:00:38 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(kook(4,44));
+        System.out.println(kook(50,4));
+        System.out.println(kook(22,45));
+        System.out.println(kook(56,4));
+        System.out.println(kook(8,56));
+    }
+    public static String kook(int age, int temperature){
+        if((age > 0 && age < 45) && (temperature > -20 && temperature > 40)){
+            System.out.println("Lets walk");
+        }else if ((age < 20) && (temperature > 0 && temperature < 25)){
+            System.out.println("We can walk");
+        }else if((age < 45) && (temperature > -10 && temperature < 25)){
+            System.out.println("We can walk");
+        }else{
+            System.out.println("Stay at home");
+        }
+        return age + " " + temperature;
+    }
+}
