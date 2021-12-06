bd354467fa0e42b85c01cc152fb6c957db405bd9
Nurik <nturdukulov911@gmail.com>
2021-12-06 16:52:00 +0600

    Changes in sout and (<;>) symbols.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,22 @@
 package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(kook(4,44));
-        System.out.println(kook(50,4));
-        System.out.println(kook(22,45));
-        System.out.println(kook(56,4));
-        System.out.println(kook(8,56));
+        kook(4, 44);
+        kook(50, 4);
+        kook(22, 45);
+        kook(56, 4);
+        kook(8, 56);
     }
-    public static String kook(int age, int temperature){
-        if((age > 0 && age < 45) && (temperature > -20 && temperature > 40)){
+
+    public static String kook(int age, int temperature) {
+        if ((age > 0 && age < 45) && (temperature > -20 && temperature < 40)) {
             System.out.println("Lets walk");
-        }else if ((age < 20) && (temperature > 0 && temperature < 25)){
+        } else if ((age < 20) && (temperature > 0 && temperature < 25)) {
             System.out.println("We can walk");
-        }else if((age < 45) && (temperature > -10 && temperature < 25)){
+        } else if ((age > 45) && (temperature > -10 && temperature < 25)) {
             System.out.println("We can walk");
-        }else{
+        } else {
             System.out.println("Stay at home");
         }
         return age + " " + temperature;
